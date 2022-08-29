d79a3d94add33bf09d55d135b28c176a3a846cca
CHER EN YAP <87885004+TheJefYCE@users.noreply.github.com>
2022-08-29 11:49:39 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+customhost = "employee.camlvzqvybsw.us-east-1.rds.amazonaws.com"
+
+customuser = "aws_admin"
+
+custompass = "Bait3273"
+
+customdb = "employee"
+
+custombucket = "yapce-employee"
+
+customregion = "us-east-1"
